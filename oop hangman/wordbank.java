6640db4d785b1d988c7c27704b08b37ddfa5fb83
gurupatoh <39097684+gurupatoh@users.noreply.github.com>
2020-12-04 23:35:31 +0300

    second commit

--- oop hangman/wordbank.java
+++ oop hangman/wordbank.java
@@ -16,7 +16,7 @@ public class wordbank {
     }
 
     public void display() {
-        String[] category = {"sports"};
+        String[] category = {"sports","fruits","vehicles"};
         System.out.println("select a category to proceed:");
         for (String element : category) {
             System.out.println(element);
@@ -31,26 +31,15 @@ public class wordbank {
         Random random = new Random();
         // word bank initialized
         String[] sports = {"football", "volleyball", "handdball", "cricket", "basketball"};
-        // letter word bank
-        String[] letterBank = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
-                "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-        //blank list for remaining words int the word bank for choices
-        String[] usedLetters = new String[26];
-
-        //Random letter choice for later
-        int randomLetter;
-        //ask for input and sets that equal to the user's guess for the letter
-        //String letterGuess = input.nextString();
-        String letterGuess = "a";
-        //counts index position of used letter
-        int usedPosition = 0;
+        String[] fruits = {"bananas", "grapes", "apples", "oranges", "mangos"};
+        String[] vehicles = {"car", "van", "lorry", "pickup", "truck"};
 
 
-        System.out.println("select  a category:");
+         System.out.println("select  a category:");
         String userChoice = scanner.nextLine();
-
-        if (userChoice.equals("sports")) {
-            int randomNumber = random.nextInt(10); // random.nextInt(10);
+        switch (userChoice) {
+            case "sports":
+            int randomNumber = random.nextInt(6); // random.nextInt(10);
             char[] randomWordToGuess = sports[randomNumber].toCharArray(); // java -> j,a,v,a
 
             int ammountOfGuesses = randomWordToGuess.length; // total tries to guess a word.
@@ -63,6 +52,96 @@ public class wordbank {
 
             boolean weArePlaying = true;
             while (weArePlaying) {
+                System.out.println("Lets Start Pl5aying Hangman ver 0.1");
+
+                while (!wordIsGuessed && tries != ammountOfGuesses) {
+                    System.out.println("Current Guesses: ");
+                    print(playerGuess);
+                    System.out.printf("You have %d amount of tries left.\n", ammountOfGuesses - tries);
+     
+                    System.out.println("Enter a single character: ");
+                    char input = scanner.nextLine().charAt(0);
+                                     // letter word bank
+        char [] letterBank = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        
+
+            //removing letter already selected
+            for(int i = 0; i < letterBank.length; i++){
+                if(letterBank[i] == input){
+                    // shifting elements
+                    for(int j = i; j < letterBank.length - 1; j++){
+                        letterBank[j] = letterBank[j+1];
+                    }
+                    break;
+                }
+            }
+            System.out.println("Letters -- " );
+            for(int i = 0; i < letterBank.length - 1; i++){
+                System.out.print(" " + letterBank[i]);
+            }  
+          
+            
+ System.out.println("\n");
+
+                    tries++;
+                    hangmanImage();
+
+                    if (input == '-') {
+                        wordIsGuessed = true;
+                        weArePlaying = false;
+                        System.out.println("entered the wrong letter!");
+
+
+                    }                        
+
+                     else {
+                        for (int i = 0; i < randomWordToGuess.length; i++) {
+                            if (randomWordToGuess[i] == input) {
+                                playerGuess[i] = input;
+                            }
+                        }
+
+                        if (isTheWordGuessed(playerGuess)) {
+                            wordIsGuessed = true;
+                            System.out.println("Congratulations");
+                        }
+                    }
+                } /* End of wordIsGuessed */
+
+                if (!wordIsGuessed) {
+                    System.out.println("You ran out of guesses.");
+                }
+
+
+                System.out.println("Would you like to play again? (yes/no) ");
+                String choice = scanner.nextLine();
+                if (choice.equals("no")) {
+                    weArePlaying = false;
+                } else if (choice.equals("yes")) {
+                    weArePlaying = true;
+
+                }
+
+            } /* End of We Are Playing */
+            scanner.close();
+
+            System.out.println("Game Over!");
+        
+                break;
+                case "fruits":
+                randomNumber = random.nextInt(6); // random.nextInt(10);
+             randomWordToGuess = fruits[randomNumber].toCharArray(); // java -> j,a,v,a
+
+         ammountOfGuesses = randomWordToGuess.length; // total tries to guess a word.
+             playerGuess = new char[ammountOfGuesses]; // "_ _ _ _ _ _ _ _"
+
+            // Assign empty dashes at start "_ _ _ _ _ _ _ _"
+            Arrays.fill(playerGuess, '_');
+
+            wordIsGuessed = false;
+
+            weArePlaying = true;
+            while (weArePlaying) {
                 System.out.println("Lets Start Playing Hangman ver 0.1");
 
                 while (!wordIsGuessed && tries != ammountOfGuesses) {
@@ -71,20 +150,102 @@ public class wordbank {
                     System.out.printf("You have %d amount of tries left.\n", ammountOfGuesses - tries);
                     System.out.println("Enter a single character: ");
                     char input = scanner.nextLine().charAt(0);
-                    //gets a random letter to use in guessing
-                    randomLetter = (int) (Math.random() * letterBank.length);
-                    letterGuess = letterBank[randomLetter];
-                    for (int i = 0; i < usedLetters.length; i++) {
-                        if (letterGuess.equals(usedLetters[i])) {
-                            randomLetter = (int) (Math.random() * letterBank.length);
-                            letterGuess = letterBank[randomLetter];
-                            i = 0;
+                                                     // letter word bank
+        char [] letterBank = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        
+
+        //removing letter already selected
+        for(int i = 0; i < letterBank.length; i++){
+            if(letterBank[i] == input){
+                // shifting elements
+                for(int j = i; j < letterBank.length - 1; j++){
+                    letterBank[j] = letterBank[j+1];
+                }
+                break;
+            }
+        }
+        System.out.println("Letters -- " );
+        for(int i = 0; i < letterBank.length - 1; i++){
+            System.out.print(" " + letterBank[i]);
+        } 
+
+                    tries++;
+
+                    if (input == '-') {
+                        wordIsGuessed = true;
+                        weArePlaying = false;
+                    } else {
+                        for (int i = 0; i < randomWordToGuess.length; i++) {
+                            if (randomWordToGuess[i] == input) {
+                                playerGuess[i] = input;
+                            }
+                        }
+
+                        if (isTheWordGuessed(playerGuess)) {
+                            wordIsGuessed = true;
+                            System.out.println("Congratulations");
                         }
                     }
-                    usedLetters[usedPosition] = letterGuess;
-                    usedPosition += 1;
-                    System.out.println("The letter guess is: " + letterGuess);
-                    //Checks if both the found letters and the letters in the original word are the same. If so, you win!
+                } /* End of wordIsGuessed */
+                if (!wordIsGuessed) {
+                    System.out.println("You ran out of guesses.");
+                }
+
+                System.out.println("Would you like to play again? (yes/no) ");
+                String choice = scanner.nextLine();
+                if (choice.equals("no")) {
+                    weArePlaying = false;
+                } else if (choice.equals("yes")) {
+                    weArePlaying = true;
+
+                }
+
+            } /* End of We Are Playing */
+            scanner.close();
+
+            System.out.println("Game Over!");
+        
+                break;
+                case "vehicles":
+             randomNumber = random.nextInt(6); // random.nextInt(10);
+             randomWordToGuess = vehicles[randomNumber].toCharArray(); // java -> j,a,v,a
+
+           ammountOfGuesses = randomWordToGuess.length; // total tries to guess a word.
+             playerGuess = new char[ammountOfGuesses]; // "_ _ _ _ _ _ _ _"
+
+            // Assign empty dashes at start "_ _ _ _ _ _ _ _"
+            Arrays.fill(playerGuess, '_');
+
+             wordIsGuessed = false;
+
+             weArePlaying = true;
+            while (weArePlaying) {
+                System.out.println("Lets Start Playing Hangman ver 0.1");
+
+                while (!wordIsGuessed && tries != ammountOfGuesses) {
+                    System.out.println("Current Guesses: ");
+                    print(playerGuess);
+                    System.out.printf("You have %d amount of tries left.\n", ammountOfGuesses - tries);
+                    System.out.println("Enter a single character: ");
+                    char input = scanner.nextLine().charAt(0);
+                                                     // letter word bank
+        char [] letterBank = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        
+
+        //removing letter already selected
+        for(int i = 0; i < letterBank.length; i++){
+            if(letterBank[i] == input){
+                // shifting elements
+                for(int j = i; j < letterBank.length - 1; j++){
+                    letterBank[j] = letterBank[j+1];
+                }
+                break;
+            }
+        }
+        System.out.println("Letters -- " );
+        for(int i = 0; i < letterBank.length - 1; i++){
+            System.out.print(" " + letterBank[i]);
+        } 
 
                     tries++;
 
@@ -123,7 +284,11 @@ public class wordbank {
 
             System.out.println("Game Over!");
         }
+                
+                
     }
+
+      
 
     public static void print(char array[]) {
         for (int i = 0; i < array.length; i++) { // Assign empty dashes at start "_ _ _ _ _ _ _ _"
